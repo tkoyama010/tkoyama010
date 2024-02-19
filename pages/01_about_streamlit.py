ebe289f68d70212ff99831ba8e9fdd8d255836e4
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-20 07:14:02 +0900

    Add sidebar example to show streamlit widgets

--- pages/01_about_streamlit.py
+++ pages/01_about_streamlit.py
@@ -2,4 +2,14 @@ from pathlib import Path
 
 import streamlit as st
 
+with st.sidebar:
+    genre = st.radio(
+        "What's your favorite movie genre",
+        [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+        captions=["Laugh out loud.", "Get the popcorn.", "Never stop learning."],
+    )
+    option = st.selectbox(
+        "How would you like to be contacted?", ("Email", "Home phone", "Mobile phone")
+    )
+
 st.markdown(Path("pages/01_about_streamlit.md").read_text(), unsafe_allow_html=True)
