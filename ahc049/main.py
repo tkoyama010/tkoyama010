8a37bdc66d09b8e0373d6953bc4bd25025fb3296
Tetsuo Koyama <tkoyama010@gmail.com>
2025-06-21 15:46:27 +0900

    chore

--- ahc049/main.py
+++ ahc049/main.py
@@ -18,8 +18,8 @@ def read_input():
         int: The size n of the grid.
     """
     n = int(input())
-    w = [list(map(int, input().split())) for _ in range(n)]
-    d = [list(map(int, input().split())) for _ in range(n)]
+    # w = [list(map(int, input().split())) for _ in range(n)]
+    # d = [list(map(int, input().split())) for _ in range(n)]
     return n
 
 
