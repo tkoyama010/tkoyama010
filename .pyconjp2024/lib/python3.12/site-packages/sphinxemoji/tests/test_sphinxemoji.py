c533bbe769622fab464465d5f5c806cdf84a24b8
Tetsuo Koyama <tkoyama010@gmail.com>
2025-12-30 10:41:50 +0900

    Remove .pyconjp2024 directory

--- .pyconjp2024/lib/python3.12/site-packages/sphinxemoji/tests/test_sphinxemoji.py
+++
@@ -1,5 +0,0 @@
-from sphinxemoji.sphinxemoji import load_emoji_codes
-
-def test_load_emoji_codes():
-    x = load_emoji_codes()
-    assert 0
--- .pyconjp2024/lib/python3.12/site-packages/trame/tools/vtksz2html.py
+++
@@ -1,4 +0,0 @@
-from trame_vtk.tools.vtksz2html import main
-
-if __name__ == "__main__":
-    main()
